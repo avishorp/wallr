21467061d7d8eb3833ed4f67cdeb784161674648
Avishay Orpaz <avishorp@gmail.com>
2015-05-13 08:42:19 +0300

    Add Wallr logo

--- src/WallrResources.py
+++ src/WallrResources.py
@@ -6,10 +6,16 @@ RESOURCE_DIR = "../res"
 GameResource.setResourceBaseDir(RESOURCE_DIR)
 
 RESOURCES = {
+    'logo': GameResource.ImageResource('logo.png', keycolor = (255, 255, 255)),
     'fuel_gauge': GameResource.ImageResource('fuel_gauge.png', center=(100, 100),
                                 keycolor = (255,255,255),
                                 needle_pos = (132, 137)),
     'needle': GameResource.ImageResource('needle.png', 
                             center = (105, 4), keycolor = (255, 255, 255))
+}
+
+SETTINGS = {
+    'screen_size': (640, 480),
+    'background_color': (255, 255, 255)
 }
 
